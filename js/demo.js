60717f863c25472ed5e62fbf8b0c1d0904807d75
Sam Vitello <sam.j.vitello@gmail.com>
2017-03-04 21:08:22 -0500

    wip

--- js/demo.js
+++ js/demo.js
@@ -1,23 +1,66 @@
 function consultationKitDemo() {
+
     var addHour = function (date, amount) {
-        return new Date(date.valueOf() + amount * (1000 * 60 * 60));
+         return new Date(date.valueOf() + amount * (1000 * 60 * 60));
+     };
+
+     var createFakeEvents = function (amount) {
+         var firstDate = toNearestHour(new Date());
+
+         var times = [];
+
+         for (var i = 0; i < amount; i++) {
+             times.push({
+                 start: addHour(firstDate, i).toISOString(),
+                 end: addHour(firstDate, i + 1).toISOString()
+             });
+         }
+         console.log(times);
+         return {data: times};
+     };
+
+    var addDay = function (date, amount) {
+        return new Date(date.valueOf() + amount * (1000 * 60 * 60 * 24));
     };
     var toNearestHour = function (date) {
         return new Date(date.valueOf() - (date.valueOf() % (1000 * 60 * 60)));
     };
 
-    var createFakeEvents = function (amount) {
+    function RFC3339DateString(d){
+       function pad(n){return n<10 ? '0'+n : n}
+       return d.getUTCFullYear()+'-'
+            + pad(d.getUTCMonth()+1)+'-'
+            + pad(d.getUTCDate())+'T'
+            + pad(d.getUTCHours())+':'
+            + pad(d.getUTCMinutes())+':'
+            + pad(d.getUTCSeconds())+'Z'
+    }
+
+    function addToTimes(availabilities, times) {
+        for (var i = 0; i < availabilities.length; i++) {
+          times.push({
+            start: availabilities[i].start_datetime,
+            end: availabilities[i].end_datetime
+          })
+        }
+    }
+
+    function getAvailabilities(days, times) {
         var firstDate = toNearestHour(new Date());
 
-        var times = [];
+        for (var i = 0; i < days; i++) {
+          start_datetime = RFC3339DateString(addDay(firstDate, i))
 
-        for (var i = 0; i < amount; i++) {
-            times.push({
-                start: addHour(firstDate, i).toISOString(),
-                end: addHour(firstDate, i + 1).toISOString()
-            });
+          $.ajax({'url': 'http://localhost:5000/calendars/1/availabilities?start_datetime=' + start_datetime, 'type':'GET',
+                  'headers': {
+                    "authorization": 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODkwMjk2MzUwMDAsInVzZXJfaWQiOjF9.ZKAou1oJ6aI93Wp7ooqSgTsQ65v97J22YGrBwvQf0cw'
+                  },
+                  success: function(result) {
+                    addToTimes(result.availabilities, times)
+                  }
+                }
+              );
         }
-        return {data: times};
     };
 
     flBooking({
@@ -77,7 +120,14 @@ function consultationKitDemo() {
             return Promise.resolve(data)
         }, // must return a promise
         getEvents: function (data) {
-            return createFakeEvents(1000)
+            // return {data: [{"start": "2017-03-09T14:00:00Z", "end": "2017-03-09T14:15:00Z"}]}
+            let times = [];
+            const availPromises = getAvailabilities(7, times);
+
+            return $.when(availPromises).done(function() {
+              console.log(times);
+              return {data: times};
+            });
         },
         callbacks: {
             showBookingPage: function (slot) {
